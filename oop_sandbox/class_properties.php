77fa2e82c53d335146f985455341c0a836613051
Wladimir Termont <w.termont@freightways.local>
2019-01-16 15:49:10 +0100

    9

--- oop_sandbox/class_properties.php
+++ oop_sandbox/class_properties.php
@@ -14,7 +14,8 @@ $student2 = new Student;
 $student2->first_name = "Jaap";
 $student2->last_name = "Vermeulen";
 
-echo $student1->first_name . $student1->last_name
-echo $student2->first_name . $student2->last_name
+echo $student1->first_name . $student1->last_name;
+echo $student2->first_name . $student2->last_name;
+echo "Test"
 
 ?>
