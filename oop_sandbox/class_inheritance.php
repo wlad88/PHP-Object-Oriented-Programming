535d5f55b5bd7023059cc9fe67bb86fbd0b4f9c5
Wladimir Termont <w.termont@freightways.local>
2019-01-29 14:39:59 +0100

    25

---
+++ oop_sandbox/class_inheritance.php
@@ -0,0 +1,42 @@
+<?php
+
+  class User {
+
+    var first_name;
+    var last_name;
+    var username;
+
+    funtion full_Name() {
+      return $this->first_name . "" . $this->last_name;
+    }
+
+    class Customer extends User {
+
+    }
+
+    $u = new User;
+    $u->first_name = 'Jerry';
+    $u->last_name = 'Seinfeld';
+    $u->username = 'jseinfeld';
+
+    $c = new Customer;
+    $c->first_name = 'George';
+    $c->last_name = 'Constanza';
+    $c->username = 'gconstanza';
+
+    echo $u->full_Name() . "<br />";
+    echo $c->full_Name() . "<br />";
+
+    echo get_parent_class($u) . "<br />";
+    echo get_parent_class($c) . "<br />";
+
+    if (is_subclass_of($c, 'User')) {
+      echo "Instance is a subclass of User.<br />";
+    }
+
+    $parents = class_parents($c);
+    echo $parents . "<br />";
+
+  }
+
+?>
